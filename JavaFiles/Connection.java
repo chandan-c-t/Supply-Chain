475296e492bbd1164faa61cbb1f0433533d4dbcb
CHANDAN <119494006+chandan-c-t@users.noreply.github.com>
2022-11-30 16:34:32 +0530

    Update Connection.java

--- JavaFiles/Connection.java
+++ JavaFiles/Connection.java
@@ -5,7 +5,7 @@ import java.sql.*;
 public class Connection {
     String url = "jdbc:mysql://localhost:3306/project";
     String user = "root";
-    String password = "Chandu@777";
+    String password = "*******";
    public java.sql.Connection con = null;
 
   public  Connection(){
